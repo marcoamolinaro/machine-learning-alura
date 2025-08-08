f6c10f6802669f2ad0f10d2e7e170bbe14740880
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2025-08-08 07:51:04 -0300

    Adicionando elementos no gráfico

--- codigo/app.py
+++ codigo/app.py
@@ -9,16 +9,30 @@ dados = heart_disease.data.features
 
 # Create histogram figure (no .show() here!)
 figura_histograma = px.histogram(dados, x='age', title='Histograma de idade')
+div_do_histograma = html.Div([
+        html.H2("Histograma de Idades"),
+        dcc.Graph(figure=figura_histograma)
+    ])
+
+dados['doenca'] = (heart_disease.data.targets > 0) * 1
+figura_boxplot = px.box(dados, x='doenca', y='age', title='Boxplot de Idades', color='doenca')
+div_do_boxplot = html.Div([
+        html.H2("Boxplot de Idades"),
+        dcc.Graph(figure=figura_boxplot)
+    ])
 
 # Dash app
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1("Análise de dados do UCI Repository Heart Disease"),
-    dcc.Graph(figure=figura_histograma)
+    div_do_histograma,
+    div_do_boxplot
 ])
+
+# podemos adicionar de forma dinâmica mais elementos no gráfico
+#app.layout.children.append(div_do_boxplot)
 
 # Run app
 if __name__ == '__main__':
     app.run(debug=True)
-    

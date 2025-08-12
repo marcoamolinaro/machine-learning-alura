3855d1928565fe5d681e98cf5d4b704873e299ee
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2025-08-12 19:52:19 -0300

    Adicionando style.css

--- codigo/app_formulario.py
+++ codigo/app_formulario.py
@@ -15,11 +15,11 @@ formulario = dbc.Container([
         dbc.Row([
             dbc.Col([
                 dbc.CardGroup([
-                    dbc.Label("Idade"),
-                    dbc.Input(id='idade', type='number',placeholder='Digite a idade'),
+                    dbc.Label("Idade", class_name='custom-label'),
+                    dbc.Input(id='idade', type='number', placeholder='Digite a idade', className='mb-3 custom-input'),
                 ], className='mb-3'),
                 dbc.CardGroup([
-                dbc.Label("Sexo Biológico"),
+                dbc.Label("Sexo Biológico", class_name='custom-label'),
                     dbc.Select(id='sexo', options=[
                         {'label': 'Masculino', 'value': '1'},
                         {'label': 'Feminino', 'value': '0'}
@@ -27,7 +27,7 @@ formulario = dbc.Container([
                 ], className='mb-3'),
                 # tipo de dor no peito
                 dbc.CardGroup([
-                    dbc.Label("Tipo de dor no peito"),
+                    dbc.Label("Tipo de dor no peito", class_name='custom-label'),
                     dbc.Select(id='cp', options=[
                         {'label': 'Angina típica', 'value': '1'},
                         {'label': 'Angina atípica', 'value': '2'},
@@ -37,37 +37,37 @@ formulario = dbc.Container([
                 ]),
                 # trestbps
                 dbc.CardGroup([
-                    dbc.Label('Pressão arterial em repouso'),
-                    dbc.Input(id='trestbps', type='number', placeholder='Digite a pressão arterial')
+                    dbc.Label('Pressão arterial em repouso', class_name='custom-label'),
+                    dbc.Input(id='trestbps', type='number', placeholder='Digite a pressão arterial', className='mb-3 custom-input')
                 ], className='mb-3'),
                 # chol
                 dbc.CardGroup([
-                    dbc.Label('Colesterol sério'),
-                    dbc.Input(id='chol', type='number', placeholder='Digite a colesterol')
+                    dbc.Label('Colesterol sério', class_name='custom-label'),
+                    dbc.Input(id='chol', type='number', placeholder='Digite a colesterol', className='mb-3 custom-input')
                 ], className='mb-3'),
                 # fbs
                 dbc.CardGroup([
-                    dbc.Label('Glicose em jejum'),
+                    dbc.Label('Glicose em jejum', class_name='custom-label'),
                     dbc.Select(id='fbs', options=[
                         {'label': 'Menor que 120 mg/dl', 'value': '0'},
                         {'label': 'Maior que 120 mg/dl', 'value': '1'},
-                    ], className='mb-3'),
+                    ], className='mb-3 custom-select'),
                 ]),
                 # restecg
                 dbc.CardGroup([
-                    dbc.Label("Eletrocardiografia em repouso"),
+                    dbc.Label("Eletrocardiografia em repouso", class_name='custom-label'),
                     dbc.Select(id='restecg', options=[
                         {'label': 'Normal', 'value': '0'},
                         {'label': 'Anormalidades de ST-T', 'value': '1'},
                         {'label': 'Hipertrofia ventricular esquerda', 'value': '2'}
                     ]),
-                ], className='mb-3'),
+                ], className='mb-3 custom-select'),
             ]),
             dbc.Col([
                 # thalach
                 dbc.CardGroup([
-                    dbc.Label('Frequência cardíaca máxima'),
-                    dbc.Input(id='thalach', type='number', placeholder='Digite a frequência cardíaca máxima')
+                    dbc.Label('Frequência cardíaca máxima', class_name='custom-label'),
+                    dbc.Input(id='thalach', type='number', placeholder='Digite a frequência cardíaca máxima', className='mb-3 custom-input')
                 ], className='mb-3'),
                 # exang
                 dbc.CardGroup([
@@ -75,40 +75,41 @@ formulario = dbc.Container([
                     dbc.Select(id='exang', options=[
                         {'label': 'Sim', 'value': '1'},
                         {'label': 'Não', 'value': '0'},
-                    ], className='mb-3'),
+                    ], className='mb-3 custom-select'),
                 ]), 
                 # oldpeak
                 dbc.CardGroup([
-                    dbc.Label('Depressão do segmento ST induzida por exercício'),
-                    dbc.Input(id='oldpeak', type='number', placeholder='Digite a depressão do segmento ST induzida por exercício')
+                    dbc.Label('Depressão do segmento ST induzida por exercício', class_name='custom-label'),
+                    dbc.Input(id='oldpeak', type='number', placeholder='Digite a depressão do segmento ST induzida por exercício',
+                               className='mb-3 custom-input')
                 ], className='mb-3'),
                 # slope
                 dbc.CardGroup([
-                    dbc.Label("Inclinação do segmento ST"),
+                    dbc.Label("Inclinação do segmento ST", class_name='custom-label'),
                     dbc.Select(id='slope', options=[
                         {'label': 'Ascendente', 'value': '1'},
                         {'label': 'Plana', 'value': '2'},
                         {'label': 'Descendente', 'value': '3'},
-                    ], className='mb-3'),
+                    ], className='mb-3 custom-select'),
                 ]),
                 # ca
                 dbc.CardGroup([
-                    dbc.Label('Número de vasos principais coloridos por fluoroscopia'),
+                    dbc.Label('Número de vasos principais coloridos por fluoroscopia', class_name='custom-label'),
                     dbc.Select(id='ca', options=[
                         {'label': '0', 'value': '0'},
                         {'label': '1', 'value': '1'},
                         {'label': '2', 'value': '2'},
                         {'label': '3', 'value': '3'},
-                    ], className='mb-3'),
+                    ], className='mb-3 custom-select'),
                 ], className='mb-3'),    
                 # thal
                     dbc.CardGroup([
-                    dbc.Label('Cintilografia do miocardio'),
+                    dbc.Label('Cintilografia do miocardio', class_name='custom-label'),
                     dbc.Select(id='thal', options=[
                         {'label': 'Normal', 'value': '3'},
                         {'label': 'Defeito fixo', 'value': '6'},
                         {'label': 'Defeito reversível', 'value': '7'},
-                    ], className='mb-3'),
+                    ], className='mb-3 custom-select'),
                 ], className='mb-3'),    
                 # botão de submit para previsão
                 dbc.Button("Prever", id='botao-prever', color='success', n_clicks=0, className='mb-3 mt-3'),
@@ -117,8 +118,8 @@ formulario = dbc.Container([
     ], fluid=True)
 
 app.layout = html.Div([
-    html.H1("Previsão de doença cardíaca", className='text-center mt-5'),
-    html.P("Prencha as informações abaixo e clique em PREVER para rodar o modelo", className='text-center mb-5'),
+    html.H1("Previsão de doença cardíaca", className="text-center mt-5 custom-title"),
+    html.P("Prencha as informações abaixo e clique em PREVER para rodar o modelo", className="text-center mb-5 custom-subtitle"),
     formulario,
     html.Div(id='previsao')
 ])
